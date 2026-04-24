3d261a53c0125863027f1b391c680b0859a7ff03
VitEfimov <135293952+VitEfimov@users.noreply.github.com>
2026-04-24 01:05:32 -0400

    same

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,46 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     webpack: (config, { isServer }) => {
+//         // Fix for alasql react-native errors
+//         config.externals = {
+//             ...config.externals,
+//             'react-native': 'undefined',
+//             'react-native-fs': 'undefined',
+//             'react-native-fetch-blob': 'undefined',
+//         };
+
+//         config.resolve.fallback = {
+//             ...config.resolve.fallback,
+//             "react-native": false,
+//             "react-native-fs": false,
+//             "react-native-fetch-blob": false,
+//             "fs": false,
+//             "crypto": false,
+//             "os": false,
+//             "path": false,
+//         };
+
+//         return config;
+//     },
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (config, { isServer }) => {
         // Fix for alasql react-native errors
-        config.externals = {
-            ...config.externals,
-            'react-native': 'undefined',
-            'react-native-fs': 'undefined',
-            'react-native-fetch-blob': 'undefined',
+        // Setting an alias to false tells Webpack to ignore the module entirely
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'react-native': false,
+            'react-native-fs': false,
+            'react-native-fetch-blob': false,
         };
 
         config.resolve.fallback = {
             ...config.resolve.fallback,
-            "react-native": false,
-            "react-native-fs": false,
-            "react-native-fetch-blob": false,
             "fs": false,
             "crypto": false,
             "os": false,
