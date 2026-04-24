bcb3bd20624f89801dae08c5b65be462a0964ce2
VitEfimov <135293952+VitEfimov@users.noreply.github.com>
2026-04-24 00:52:15 -0400

    next.config modified

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,14 @@
 const nextConfig = {
     webpack: (config, { isServer }) => {
         // Fix for alasql react-native errors
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            alasql: 'alasql/dist/alasql.min.js',
+        };
+
         config.resolve.fallback = {
             ...config.resolve.fallback,
+            "react-native": false,
             "react-native-fs": false,
             "react-native-fetch-blob": false,
             "fs": false,
